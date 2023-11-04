a110621aad0db0336eb242b96f687ff1a998eace
kyleespera <kyleespera134@gmail.com>
2023-11-04 04:16:51 -0700

    Modularization, Descriptive Function Names,

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,41 +1,64 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const {
+  warmStrategyCache,
+  CacheFirst,
+  StaleWhileRevalidate,
+} = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-precacheAndRoute(self.__WB_MANIFEST);
+// Precaches files.
+function setupPrecaching() {
+  precacheAndRoute(self.__WB_MANIFEST);
+}
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-registerRoute(
-  
-({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: "asset-cache",
+// Returns a CacheFirst strategy for page caching.
+function createPageCacheStrategy() {
+  return new CacheFirst({
+    cacheName: 'page-cache',
     plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
+      new CacheableResponsePlugin({ statuses: [0, 200] }),
+      new ExpirationPlugin({ maxAgeSeconds: 30 * 24 * 60 * 60 }), // 30 Days
     ],
-  })
-);
+  });
+}
+
+// Warms the cache with frequently used URLs.
+function warmCache(urls, strategy) {
+  warmStrategyCache({ urls, strategy });
+}
+
+// Registers route with navigation request strategy.
+function registerNavigationRoute(strategy) {
+  registerRoute(({ request }) => request.mode === 'navigate', strategy);
+}
+
+// Registers route for caching assets like JS, CSS, and worker scripts.
+function registerAssetsCache() {
+  registerRoute(
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      cacheName: 'asset-cache',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [0, 200] }),
+        new ExpirationPlugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    })
+  );
+}
+
+// Setting up the service worker caching strategies.
+function setupCachingStrategies() {
+  const pageCacheStrategy = createPageCacheStrategy();
+  setupPrecaching();
+  warmCache(['/index.html', '/'], pageCacheStrategy);
+  registerNavigationRoute(pageCacheStrategy);
+  registerAssetsCache();
+}
+
+// Initialize the service worker caching strategies.
+setupCachingStrategies();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,31 +1,89 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-const WorkboxPlugin = require("workbox-webpack-plugin");
-// TODO: Add CSS loaders and babel to webpack.
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// Entry configuration
+function getEntryConfig() {
+  return {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+    database: './src/js/database.js',
+    editor: './src/js/editor.js',
+    header: './src/js/header.js',
+  };
+}
+
+// Output configuration
+function getOutputConfig() {
+  return {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  };
+}
+
+// Plugins configuration
+function getPluginsConfig() {
+  return [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'JATE',
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'Just another text editor',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ];
+}
+
+// Module rules configuration
+function getModuleRulesConfig() {
+  return [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+        },
+      },
+    },
+  ];
+}
+
+// Webpack configuration
 module.exports = () => {
   return {
     mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
+    entry: getEntryConfig(),
+    output: getOutputConfig(),
+    plugins: getPluginsConfig(),
     module: {
-      rules: [
-        
-      ],
+      rules: getModuleRulesConfig(),
     },
   };
 };
